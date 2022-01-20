1fe31c78dabb64d3a8153ef90a2c827ae0e9481e
Harish Rajora <harrylitterell@yahoo.in>
2022-01-21 01:08:03 +0530

    Project created with default settings

---
+++ markmywords/markmywords/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for markmywords project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'markmywords.settings')
+
+application = get_asgi_application()
---
+++ markmywords/markmywords/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for markmywords project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'markmywords.settings')
+
+application = get_wsgi_application()
